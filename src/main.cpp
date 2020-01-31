047c8e19c36f5641f25b45dbcec2fe54158bdfd3
joe dao <joedao55@gmail.com>
2020-01-30 22:13:06 -0500

    now outputs as png, and processing of pixels is done in chunks

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <bits/stdint-uintn.h>
+#include <cstddef>
 #include <cstdint>
 #include <utility>
 #include <random>
@@ -13,14 +15,15 @@
 #include "util.hpp"
 #include "hitable/bvh.hpp"
 #include "material/material.hpp"
+#include "../lodepng/lodepng.h"
 
 using vec3 = geometry::vec<float,3>;
 using hitable_ptr = std::shared_ptr<hitable>;
 
 
-inline uint tocolor(const float n)
+uint8_t tocolor(const float n)
 {
-    return uint(255.99*sqrt(n));
+    return uint8_t(255.99*sqrt(n));
 }
 
 
@@ -39,7 +42,7 @@ std::unique_ptr<hitable_list> two_perlin()
 }
 
 
-inline vec3 colour(const geometry::Ray &r, hitable & world, int depth, const vec3 = vec3{0,0,0})
+vec3 colour(const geometry::Ray &r, hitable & world, int depth, const vec3 = vec3{0,0,0})
 {
     hit_record rec;
     if( world.hit(r, 0.001, MAXFLOAT, rec) )
@@ -71,37 +74,37 @@ std::unique_ptr<bvhNode> random_scene()
     spherelist.emplace_back(std::make_shared<sphere>( vec3{0, -1000, 0}, 1000,
                                                       std::make_shared<lambertian>(
                                                           std::make_unique<checker_texture>(vec3{0.9, 0.9, 0.9},vec3{0.1 ,0.1, 0.1}))));
-    for(int a=-10; a<10; a++)
-    {
-        for(int  b=-10; b<10; b++)
-        {
-            float choose_mat = random_float();
-            vec3 center{a+0.9f*random_float(), 0.2, b+0.9f*random_float()};
-            if ((center - vec3{4,0.2,0}).length() > 0.9)
-            {
-                if( choose_mat <0.8)
-                    spherelist.emplace_back(std::make_shared<moving_sphere>( center, center+ vec3{0, 0.5f* random_float(), 0},
-                                                                             0.0, 1.0, 0.2,
-                                                                             std::make_shared<lambertian>(
-                                                                                 std::make_unique<const_texture>(
-                                                                                     vec3{random_float()*random_float(),
-                                                                                          random_float()*random_float(),
-                                                                                          random_float()*random_float()}))));
-                else if(choose_mat < 0.95)
-                {
-                    spherelist.emplace_back(std::make_shared<sphere>(center, 0.2,
-                                                                            std::make_shared<metal>(vec3{0.5f*(1+random_float()),
-                                                                                                         0.5f*(1+random_float()),
-                                                                                                         0.5f*(1+random_float())},
-                                                                                0.5*random_float())));
-                }
-                else {
-                    spherelist.emplace_back(std::make_shared<sphere>( center, 0.2, std::make_shared<dielectric>(1.5)));
+    // for(int a=-10; a<10; a++)
+    // {
+    //     for(int  b=-10; b<10; b++)
+    //     {
+    //         float choose_mat = random_float();
+    //         vec3 center{a+0.9f*random_float(), 0.2, b+0.9f*random_float()};
+    //         if ((center - vec3{4,0.2,0}).length() > 0.9)
+    //         {
+    //             if( choose_mat <0.8)
+    //                 spherelist.emplace_back(std::make_shared<moving_sphere>( center, center+ vec3{0, 0.5f* random_float(), 0},
+    //                                                                          0.0, 1.0, 0.2,
+    //                                                                          std::make_shared<lambertian>(
+    //                                                                              std::make_unique<const_texture>(
+    //                                                                                  vec3{random_float()*random_float(),
+    //                                                                                       random_float()*random_float(),
+    //                                                                                       random_float()*random_float()}))));
+    //             else if(choose_mat < 0.95)
+    //             {
+    //                 spherelist.emplace_back(std::make_shared<sphere>(center, 0.2,
+    //                                                                  std::make_shared<metal>(vec3{0.5f*(1+random_float()),
+    //                                                                                               0.5f*(1+random_float()),
+    //                                                                                               0.5f*(1+random_float())},
+    //                                                                      0.5*random_float())));
+    //             }
+    //             else {
+    //                 spherelist.emplace_back(std::make_shared<sphere>( center, 0.2, std::make_shared<dielectric>(1.5)));
 
-                }
-            }
-        }
-    }
+    //             }
+    //         }
+    //     }
+    // }
 
     spherelist.emplace_back(std::make_shared<sphere>( vec3{0,1,0}, 1, std::make_shared<dielectric>(1.5)));
     spherelist.emplace_back(std::make_shared<sphere>( vec3{-4,1,0}, 1, std::make_shared<dielectric>(1.5)));
@@ -110,11 +113,59 @@ std::unique_ptr<bvhNode> random_scene()
     return  bvh_list;
 }
 
+void process_block(const camera &cam,
+                   hitable & world,
+                   const int start_row,
+                   const int num_rows,
+                   const size_t nx, const size_t ny,
+                   const size_t samples,
+                   std::vector<uint8_t> & image)
+{
+    std::vector<uint8_t> block_pixels;
+    for (int j = start_row ; j>=start_row - num_rows && j >= 0 ; j--)
+    {
+
+        for (auto i = 0 ; i < nx ; i++)
+        {
+
+            vec3 col{0,0,0};
+            for(auto s=0; s<samples;s++)
+            {
+                const float u = float(i+random_float())/float(nx);
+                const float v = float(j+random_float())/float(ny);
+                const geometry::Ray r=cam.get_ray(u,v);
+
+                const auto c = colour(r, world, 0);
+                {
+                    col += c;
+                }
+            }
+            uint8_t red,g,b;
+            col /= float(samples);
+            red=tocolor(col[0]);
+            g=tocolor(col[1]);
+            b=tocolor(col[2]);
+            block_pixels.push_back(red);
+            block_pixels.push_back(g);
+            block_pixels.push_back(b);
+            block_pixels.push_back(255);//alpha channel
+
+
+        }
+    }
+    std::copy(block_pixels.begin(), block_pixels.end(), image.begin()+(ny-1-start_row)*nx*4);
+}
+
 int main()
 {
-    constexpr long int nx = 800;
-    constexpr long int ny = 500;
-    constexpr long int ns = 8;
+
+
+    constexpr auto nx = 200;
+    constexpr  auto ny = 160;
+    constexpr auto ns = 8;
+    size_t num_threads = 2;
+    size_t rows_thread = ny / num_threads;
+
     std::ofstream out_file("img.ppm");
     out_file<< "P3\n"<< nx <<" "<< ny<< "\n255\n";
 
@@ -127,33 +178,17 @@ int main()
 
     std::unique_ptr<hitable> world = random_scene();
 
-    for (auto j = ny-1 ; j >= 0 ; j--)
+    std::vector<uint8_t> image(nx*ny*4);
+
+    for(auto start_row=ny-1; start_row>=0; start_row-=rows_thread)
     {
-        for (auto i = 0 ; i < nx ; i++)
-        {
-
-            vec3 col{0,0,0};
-            #pragma omp parallel for
-            for(auto s=0; s<ns;s++)
-            {
-                const float u = float(i+random_float())/float(nx);
-                const float v = float(j+random_float())/float(ny);
-                const geometry::Ray r=cam.get_ray(u,v);
-
-                const auto c = colour(r, *world, 0);
-                #pragma omp critical
-                {
-                    col += c;
-                }
-            }
-            int red,g,b;
-            col /= float(ns);
-            red=tocolor(col[0]);
-            g=tocolor(col[1]);
-            b=tocolor(col[2]);
-            out_file<<red<<" "<<g<<" "<<b<<"\n";
-
-        }
+        process_block(cam, *world, start_row, rows_thread, nx, ny, ns, image);
     }
+
+     unsigned error = lodepng::encode("img.png", image, nx, ny);
+
+    //if there's an error, display it
+    if(error) std::cout << "encoder error " << error << ": "<< lodepng_error_text(error) << std::endl;
+
     out_file.close();
 }
