9e471d19976506fcbb61d1b90bdfdd45bc15552a
joe dao <joedao55@gmail.com>
2020-03-03 18:51:50 -0500

    added point in triangle check function with tests

--- src/geometry/geometry_util.cpp
+++ src/geometry/geometry_util.cpp
@@ -5,10 +5,18 @@
 
 using vec3 = geometry::vec<float, 3>;
 
-bool geometry::point_in_triangle(const vec3 &a, const vec3 &b, const vec3 &c, const vec3 &p)
+bool geometry::point_in_triangle(const vec3 &a, const vec3 &b, const vec3 &c, const vec3 &p, const float error)
 {
-    // TODO
-    return true;
+    float area_abc = geometry::triangle_area(a, b, c);
+    float area_pbc = geometry::triangle_area(p, b, c);
+    float area_apc = geometry::triangle_area(a, p, c);
+    float area_abp = geometry::triangle_area(a, b, p);
+
+    float area_difference = area_abc - area_abp - area_pbc - area_apc;
+    float relative_difference = area_difference / area_abc;
+    if (abs(relative_difference) < error)
+        return true;
+    return false;
 }
 float geometry::triangle_area(const vec3 &a, const vec3 &b, const vec3 &c)
 {
@@ -17,7 +25,7 @@ float geometry::triangle_area(const vec3 &a, const vec3 &b, const vec3 &c)
 
     return length(cross(ab, ac)) / 2;
 }
-std::optional<vec3> geometry::ray_plane_intersection(const Ray &r, vec3 p_point, vec3 p_normal)
+std::optional<vec3> geometry::ray_plane_intersection(const geometry::Ray &r, vec3 p_point, vec3 p_normal)
 {
     auto denominator = geometry::dot(p_normal, r.B);
     if (denominator > 1e-6)
