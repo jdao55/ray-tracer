1942622ffc8e55eeff366b07e460a687950712d3
joseph dao <joedao55@gmail.com>
2020-03-12 19:46:11 -0400

    Inlined function

--- src/util.hpp
+++ src/util.hpp
@@ -3,7 +3,7 @@
 
 #include <random>
 
-float random_float(const float min = 0.0f, const float max = 1.0f) {
+inline float random_float(const float min = 0.0f, const float max = 1.0f) {
     static thread_local std::mt19937 generator;
     std::uniform_real_distribution<float> distribution(min, max);
     return distribution(generator);
